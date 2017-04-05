5d54423567e8bba7f15205079c519f7e224ea2c9
Guido Bouman <guido@q42.nl>
2017-04-06 00:48:47 +0200

    Make CoffeeCounter reactive based on Firebase callbacks.

--- app/coffeecounter/server/coffeecounter.js
+++ app/coffeecounter/server/coffeecounter.js
@@ -2,22 +2,25 @@ import { Meteor } from 'meteor/meteor'
 import firebase from 'firebase';
 
 firebase.initializeApp({
-  databaseURL: "https://iot-api.firebaseio.com",
+  databaseURL: 'https://iot-api.firebaseio.com',
 });
-
-var coffeeRef = firebase.database().ref('coffee');
 
 const startOfDay = new Date(new Date().setHours(0,0,0,0)).toISOString();
+var coffeeRef = firebase.database().ref('coffee');
 var recentCoffee = coffeeRef.orderByChild('published_at').startAt(startOfDay);
 
-var counter = 0;
-recentCoffee.once('value', function(snapshot) {
-  snapshot.forEach(function(childSnapshot) {
+Meteor.publish('coffeeCounter', function() {
+  var counter = 0;
+  var initializing = true;
+  recentCoffee.on('child_added', (id) => {
     counter++;
-  });
-});
 
-Meteor.publish("coffeeCounter", function() {
-    this.added("coffeeCounter", null, { counter });
-    this.ready();
+    if(!initializing) {
+      this.changed('coffeeCounter', 'main', { counter });
+    }
+  });
+
+  initializing = false;
+  this.added('coffeeCounter', 'main', { counter });
+  this.ready();
 });

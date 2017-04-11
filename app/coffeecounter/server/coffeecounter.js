a86b12e922757beb2510842931201e33323ee632
Lukas van Driel <lukas@q42.nl>
2017-04-11 22:18:43 +0200

    calculate coffee once, and only send the length to the subscriber

--- app/coffeecounter/server/coffeecounter.js
+++ app/coffeecounter/server/coffeecounter.js
@@ -9,18 +9,18 @@ const startOfDay = new Date(new Date().setHours(0,0,0,0)).toISOString();
 const coffeeRef = firebase.database().ref('coffee');
 const recentCoffee = coffeeRef.orderByChild('published_at').startAt(startOfDay);
 
+let counter = [];
+recentCoffee.on('child_added', (data) => {
+  counter.push(data.val());
+});
+
+function countCoffeeToday() {
+  const coffee = counter.filter(c => c.published_at > new Date(new Date().setHours(0,0,0,0)).toISOString());
+  console.log(`Returning coffee: ${coffee.length} of ${counter.length} are from today.`);
+  return coffee.length;
+}
+
 Meteor.publish('coffeeCounter', function() {
-  let counter = 0;
-  let initializing = true;
-  recentCoffee.on('child_added', (id) => {
-    counter++;
-
-    if(!initializing) {
-      this.changed('coffeeCounter', 'main', { counter });
-    }
-  });
-
-  initializing = false;
-  this.added('coffeeCounter', 'main', { counter });
+  this.added('coffeeCounter', 'main', { counter: countCoffeeToday() });
   this.ready();
 });
